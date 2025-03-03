b8707a56716184bb52359f627ddda29ea137bf6f
YZY1360 <44240151+YZY1360@users.noreply.github.com>
2025-03-03 21:49:00 +0800

    新增wireguard, ddns-go软件包

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -42,6 +42,9 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 自定义组件
+PACKAGES="$PACKAGES luci-proto-wireguard"
+PACKAGES="$PACKAGES ddns-go"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
